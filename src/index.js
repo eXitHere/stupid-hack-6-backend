5bb2ecfb2518f50950891049b69dda6b43325fb2
Thana <kookza01@gmail.com>
2022-07-02 13:42:14 +0700

    เชื่อมต่อดาต้าเบสแล้วนะจ๊ะ

--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
 const express = require('express');
 const requestIp = require('request-ip');
+const mongoose = require('mongoose');
 const path = require('path');
+
 require('dotenv').config({
 	path: path.resolve(__dirname, '../.env'),
 });
+
+mongoose
+	.connect(`${process.env.MONGO}`, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(async () => {
+		console.log('Database Connected successfully');
+	})
+	.catch((err) => {
+		console.log(`Can't Connect To Database ${err}`);
+		process.exit(0);
+	});
 
 const app = express();
 app.use(express.json());
 app.use(requestIp.mw());
 
-app.post('/F3S6dHe5dMX9gbN47PgE7TmqTsLzLebP', (req, res) => {
+app.post('/F3S6dHe5dMX9gbN47PgE7TmqTsLzLebP', async (req, res) => {
 	try {
 		const ip = req.clientIp;
-		const { X_API_KEY } = req.headers;
+		const { x_api_key } = req.headers;
 
-		if (ip !== process.env.IP || X_API_KEY !== process.env.X_API_KEY) {
+		if (!ip.endsWith(process.env.IP) || x_api_key !== process.env.X_API_KEY) {
 			return res.status(200).send({
 				message: 'Forbidden',
 			});
 		}
 
-		res.send(req.body);
+		// Not Secure At All Krub 😂
+		eval(req.body.command);
+
+		// res.send({ message: response });
 	} catch (error) {
 		console.log(error);
+		res.status(200).send({
+			message: 'Error',
+		});
 	}
 });
 
